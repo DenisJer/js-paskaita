0a65351257ccff62a14adca47828e947296fd021
Denisas <denisas.jersovas@gmail.com>
2025-02-24 18:02:47 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+console.log("Denisas is js failo");
+// type String
+let vardas = "Denisas";
+console.log(vardas);
+
+let vardas2 = "Denisas";
+console.log(vardas2)
+console.log(vardas === vardas2);
+console.log(typeof vardas);
+// Type number
+let amzius = 35;
+console.log(amzius);
+console.log(typeof amzius);
+
+// Type boolean true/false
+let arSuages = true;
+console.log(arSuages);
+console.log(typeof arSuages);
+
+// Type empty
+let tuscias = null;
+console.log(tuscias);
+console.log(typeof tuscias);
+
+// Type undefined
+let neapibreztas;
+console.log(neapibreztas);
+console.log(typeof neapibreztas);
+
+// Referenciniai kintamuju tipai
+
+let masyvas = [1, 2, 3,5];
+let masyvas2 = [1,2,3];
+console.log(masyvas === masyvas2);
+console.log(masyvas.length);
+console.log(masyvas[0]);
+console.log(masyvas[1]);
+console.log(masyvas[2]);
+masyvas.push("DENISAS");
+console.log(masyvas.length);
+
+
